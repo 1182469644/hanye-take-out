c8d2c6cb5f9e5f2e4f12c502f1f532718c3fac68
1182469644 <1182469644@qq.com>
2025-03-08 22:19:19 +0800

    b

--- hanye-take-out-springboot3/server/src/main/java/fun/cyhgraph/ServerApplication.java
+++ hanye-take-out-springboot3/server/src/main/java/fun/cyhgraph/ServerApplication.java
@@ -7,11 +7,11 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching // 开启缓存注解功能
-@EnableScheduling // 开启定时调度功能
+@EnableCaching // 开启缓存注解功能2222
+@EnableScheduling // 开启定时调度功能11
 @Slf4j
 public class ServerApplication {
-
+//1
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
         log.info("server started successfully!");
